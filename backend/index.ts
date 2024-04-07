40e5ea7e380953b1a1d8d740c461fc325b633f20
bhataasim1 <bhataasim700@gmail.com>
2024-04-07 18:07:54 +0530

    ✨feature: Add UserRoutes to handle user API endpoints

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,17 @@
 import express, { Application } from 'express';
 import { BaseEnvironment } from './Environment';
+import cors from 'cors';
+import UserRoutes from './routes/User/User.routes';
 
 const env = new BaseEnvironment();
 
 const app: Application = express();
+const apiVersion = '/api/v1';
+
+app.use(express.json());
+app.use(cors());
+
+app.use(`${apiVersion}/user`, UserRoutes);
 
 app.get('/health-check', (req, res) => {
     res.send('I am alive!');
