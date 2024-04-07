17c179a88094ce2589ad7bdddb574f07975258a0
bhataasim1 <bhataasim700@gmail.com>
2024-04-07 18:09:03 +0530

    ✨feature: Add authentication middleware

---
+++ backend/middleware/auth.midleware.ts
@@ -0,0 +1,63 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { Request, Response, NextFunction } from 'express';
+import { BaseEnvironment } from '../Environment';
+import jwt from 'jsonwebtoken';
+
+const env = new BaseEnvironment();
+
+export type PayloadType = {
+    id: number | string;
+    email: string;
+    role?: string | number;
+};
+
+export const authMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    passport.use(
+        new JwtStrategy(
+            {
+                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                secretOrKey: env.ACCESS_TOKEN_SECRET,
+            },
+            (payload, done) => {
+                if (!payload) {
+                    return done(null, false);
+                }
+                return done(null, payload);
+            }
+        )
+    );
+    passport.authenticate('jwt', { session: false });
+    next();
+};
+
+export const generateAccessToken = (payload: PayloadType) => {
+    return jwt.sign(payload, env.ACCESS_TOKEN_SECRET, {
+        expiresIn: env.ACCESS_TOKEN_LIFE,
+    });
+};
+
+export const generateRefreshToken = (payload: PayloadType) => {
+    return jwt.sign(payload, env.REFRESH_TOKEN_SECRET);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const verifyRefreshToken = (token: any) => {
+    return jwt.verify(
+        token,
+        env.REFRESH_TOKEN_SECRET,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (err: any, user: any) => {
+            if (err) {
+                return err;
+            }
+            return user;
+        }
+    );
+};
+
+export const auth = passport.authenticate('jwt', { session: false });
