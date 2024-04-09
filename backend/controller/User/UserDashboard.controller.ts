84622de67fa03a83c3ee90e0c35ff80dafca7400
bhataasim1 <bhataasim700@gmail.com>
2024-04-09 16:05:13 +0530

    🔧 refactor: Update UserDashboard.controller.ts to use UserProfileValidator for input validation

--- backend/controller/User/UserDashboard.controller.ts
+++ backend/controller/User/UserDashboard.controller.ts
@@ -2,6 +2,7 @@ import { prisma } from '../../prisma/Schema';
 import { ApiResponse } from '../../middleware/apiResponse.middleware';
 import { Request, Response } from 'express';
 import { userAuth } from '../../middleware/ProtectedRoutes.middleware';
+import UserProfileValidator from '../../utils/Validator';
 
 export const getUserProfile = async (req: Request, res: Response) => {
     const userId = req.params.id;
@@ -68,36 +69,50 @@ export const updateProfile = async (req: Request, res: Response) => {
         pincode,
     } = req.body;
     try {
-        const user = await prisma.user.update({
-            where: {
-                id: Number(userId),
-            },
-            data: {
-                name,
-                email,
-                password,
-                avatar,
-                phone,
-                address,
-                city,
-                state,
-                pincode,
-            },
+        const validator = new UserProfileValidator(req);
+        validator
+            .validateName()
+            .ValidateEmail()
+            .validatePassword()
+            .validateAvatar()
+            .validatePhone()
+            .validateAddress()
+            .validateCity()
+            .validateState()
+            .validatePincode();
+
+        await validator.validate(req, res, async () => {
+            const user = await prisma.user.update({
+                where: {
+                    id: Number(userId),
+                },
+                data: {
+                    name,
+                    email,
+                    password,
+                    avatar,
+                    phone,
+                    address,
+                    city,
+                    state,
+                    pincode,
+                },
+            });
+            //eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { password: pass, roleId, createdAt, updatedAt, ...rest } = user;
+    
+            res.send(
+                new ApiResponse(
+                    {
+                        status: 'success',
+                        message: 'User profile updated',
+                        data: rest,
+                    },
+                    200
+                )
+            );
         });
 
-        //eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { password: pass, roleId, createdAt, updatedAt, ...rest } = user;
-
-        res.send(
-            new ApiResponse(
-                {
-                    status: 'success',
-                    message: 'User profile updated',
-                    data: rest,
-                },
-                200
-            )
-        );
     } catch (error) {
         res.send(
             new ApiResponse(
