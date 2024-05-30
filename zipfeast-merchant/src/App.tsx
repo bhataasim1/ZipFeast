32b9b6d79a1bed0c5ac962eea24a36d8d2a2fb51
bhataasim1 <bhataasim700@gmail.com>
2024-05-30 20:27:24 +0530

    feat: Add Vite configuration and necessary files for the ZipFeast Merchant app

---
+++ zipfeast-merchant/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button variant={'destructive'}>Click me</Button>
+    </>
+  )
+}
+
+export default App
