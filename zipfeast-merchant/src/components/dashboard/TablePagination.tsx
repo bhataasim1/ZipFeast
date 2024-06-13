339f90a03b388a56c7af428bf1b205bf71afc01e
bhataasim1 <bhataasim700@gmail.com>
2024-06-13 13:53:24 +0530

    refactor: Improve TablePagination component for better code organization and readability

--- zipfeast-merchant/src/components/dashboard/TablePagination.tsx
+++ zipfeast-merchant/src/components/dashboard/TablePagination.tsx
@@ -6,18 +6,52 @@ import {
 import { Button } from "../ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-const TablePagination = () => {
+type TablePaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+};
+
+const TablePagination = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: TablePaginationProps) => {
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
   return (
     <Pagination className="ml-auto mr-0 w-auto">
       <PaginationContent>
         <PaginationItem>
-          <Button size="icon" variant="outline" className="h-6 w-6">
+          <Button
+            size="icon"
+            variant="outline"
+            className="h-6 w-6"
+            onClick={handlePrevious}
+            disabled={currentPage === 1}
+          >
             <ChevronLeft className="h-3.5 w-3.5" />
             <span className="sr-only">Previous Order</span>
           </Button>
         </PaginationItem>
         <PaginationItem>
-          <Button size="icon" variant="outline" className="h-6 w-6">
+          <Button
+            size="icon"
+            variant="outline"
+            className="h-6 w-6"
+            onClick={handleNext}
+            disabled={currentPage === totalPages}
+          >
             <ChevronRight className="h-3.5 w-3.5" />
             <span className="sr-only">Next Order</span>
           </Button>
