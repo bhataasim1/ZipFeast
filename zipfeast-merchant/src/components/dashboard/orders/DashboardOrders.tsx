bd9963621f183d583f777055806dd55937e5baf1
bhataasim1 <bhataasim700@gmail.com>
2024-06-13 12:05:59 +0530

    feat: Add DashboardOrders component and OrderTable component

---
+++ zipfeast-merchant/src/components/dashboard/orders/DashboardOrders.tsx
@@ -0,0 +1,13 @@
+import OrderTable from "./OrderTable";
+
+export function DashboardOrders() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <OrderTable />
+        </main>
+      </div>
+    </div>
+  );
+}
