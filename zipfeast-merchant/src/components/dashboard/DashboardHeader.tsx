b7beb0f4f28c2a19ddd7d9411e6aae8f4e2ea10c
bhataasim1 <bhataasim700@gmail.com>
2024-06-13 14:21:32 +0530

    refactor:  commented out code and unused imports in DashboardHeader component

--- zipfeast-merchant/src/components/dashboard/DashboardHeader.tsx
+++ zipfeast-merchant/src/components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,7 @@
-import { Search } from "lucide-react";
+// import { Search } from "lucide-react";
 // import DashboardBreadCrumbs from "./DashboardBreadCrumbs";
 import DashboardMobileSidebar from "./DashboardMobileSidebar";
-import { Input } from "../ui/input";
+// import { Input } from "../ui/input";
 import { ModeToggle } from "../themes/ModeToggle";
 import { UserNav } from "../User-Nav";
 
@@ -11,12 +11,12 @@ const DashboardHeader = () => {
       <DashboardMobileSidebar />
       {/* <DashboardBreadCrumbs /> */}
       <div className="relative ml-auto flex-1 md:grow-0">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        {/* <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
           type="search"
           placeholder="Search..."
           className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-        />
+        /> */}
       </div>
       <ModeToggle />
       <UserNav />
