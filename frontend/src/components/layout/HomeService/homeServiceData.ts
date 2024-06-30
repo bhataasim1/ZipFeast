ec9818d88000870f1ca9c4bb85aa695f339f575f
bhataasim1 <bhataasim700@gmail.com>
2024-06-30 13:37:29 +0530

    feat: Add HomeService component for displaying service details

---
+++ frontend/src/components/layout/HomeService/homeServiceData.ts
@@ -0,0 +1,154 @@
+export interface HomeServiceData {
+  id: number;
+  name: string;
+  role: string;
+  image: string;
+  price: number;
+  phone: string;
+}
+
+export const homeServiceData: HomeServiceData[] = [
+  {
+    id: 1,
+    name: "Alex Jones",
+    role: "Worker",
+    image:
+      "https://img.freepik.com/free-vector/construction-worker-holding-ladder_1308-98720.jpg",
+    price: 500,
+    phone: "1234567890",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    role: "Electrician",
+    image:
+      "https://t3.ftcdn.net/jpg/05/67/79/78/360_F_567797859_CIAICt1fhN5OuxkD6ZY2eVk0dvuTTCE2.jpg",
+    price: 400,
+    phone: "1234567890",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    role: "Plumber",
+    image:
+      "https://i.pinimg.com/474x/c2/d1/ed/c2d1ed0716c40a008a1f6212ff634d48.jpg",
+    price: 300,
+    phone: "1234567890",
+  },
+  {
+    id: 4,
+    name: "Doe Parker",
+    role: "Carpenter",
+    image: "https://thumbs.dreamstime.com/b/handyman-carpenter-21780203.jpg",
+    price: 200,
+    phone: "1234567890",
+  },
+  {
+    id: 5,
+    name: "Smith Johnson",
+    role: "Painter",
+    image:
+      "https://png.pngtree.com/png-clipart/20200224/original/pngtree-classic-painter-vector-painting-wall-with-brush-house-painter-with-paintbrush-png-image_5204827.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 6,
+    name: "Johnson Williams",
+    role: "Mason",
+    image:
+      "https://media.istockphoto.com/id/610442626/photo/master-mason.jpg?s=612x612&w=0&k=20&c=8JlF8evy9RJRSup-WM6_G1XJh0Hd3tWAPUoLYERyoqk=",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 7,
+    name: "Williams Brown",
+    role: "Gardener",
+    image:
+      "https://img.freepik.com/premium-photo/male-worker-gardener-pruning-plants-flowers_7502-2265.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 8,
+    name: "Brown Davis",
+    role: "Cleaner",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/005/519/972/original/cartoon-drawing-of-a-cleaner-vector.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 9,
+    name: "Davis Wilson",
+    role: "Cook",
+    image:
+      "https://img.freepik.com/free-vector/cute-girl-character-wearing-chef-hat-cooking-food_1308-56185.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 10,
+    name: "Wilson Martinez",
+    role: "Driver",
+    image:
+      "https://info.drivedifferent.com/hubfs/SMI-BLOG-Ways-to-Improve-Drivers-Happiness%20%281%29.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 11,
+    name: "Martinez Anderson",
+    role: "Security",
+    image:
+      "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlM18zZF9jYXJ0b29uX3JlbmRlcl9vZl9hX2NvcF9fY2FydG9vbl9hbmltYXRpb25fc18zNWYxMDE5ZC0wNmQ4LTQzMGMtYTcxOC0zYzZkYjY4ZDdjNzRfMS5wbmc.png",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 12,
+    name: "Anderson Taylor",
+    role: "Tailor",
+    image:
+      "https://media.smallbiztrends.com/2023/07/how-to-become-a-tailor.png",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 13,
+    name: "Taylor Thomas",
+    role: "Barber",
+    image:
+      "https://hips.hearstapps.com/esquire/assets/17/29/1500667303-es-072117-talk-to-your-barber-about-your-hair.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 14,
+    name: "Thomas Moore",
+    role: "Hairdresser",
+    image:
+      "https://img.freepik.com/premium-photo/brutal-guy-modern-barber-shop-hairdresser-makes-hairstyle-man-with-beard-portrait-stylish-man-beard-master-hairdresser-does-hairstyle-style-with-scissors-comb_124865-7125.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 15,
+    name: "Moore Jackson",
+    role: "Plumber",
+    image:
+      "https://b3503797.smushcdn.com/3503797/wp-content/uploads/2023/01/iStock-1341381755-1024x683.jpg?lossy=2&strip=1&webp=1",
+    price: 100,
+    phone: "1234567890",
+  },
+  {
+    id: 16,
+    name: "Jackson White",
+    role: "Electrician",
+    image:
+      "https://t4.ftcdn.net/jpg/01/16/90/67/360_F_116906766_l5K1WwCz3kW8cmnpFApzIlU0gPHWjWsK.jpg",
+    price: 100,
+    phone: "1234567890",
+  },
+];
